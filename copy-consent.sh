1fcda2e91ee1b2156c357342a967281b4cd8431c
chinnawat <chinwanbbgz@gmail.com>
2021-12-15 17:12:29 +0700

    [Update Command npm move file]

---
+++ copy-consent.sh
@@ -0,0 +1,5 @@
+mv ./dist/script.min.js ../pd_dpia/WebRole/Libs/CookieConsent.v1.min.js 
+mv ./dist/style.min.css  ../pd_dpia/WebRole/Libs/CookieConsent.v1.min.css
+mv ./dist/index.html  ../pd_dpia/WebRole/Libs/index.html
+mv ./dist/preview.html  ../pd_dpia/WebRole/Libs/preview.html
+mv ./DocumentCookieConsent.txt  ../pd_dpia/WebRole/Libs/DocumentCookieConsent.txt
